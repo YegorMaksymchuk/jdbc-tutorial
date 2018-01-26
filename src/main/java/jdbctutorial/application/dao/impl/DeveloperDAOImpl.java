dc643d89cc7f5dcc9eec5de1175c222cb6c7d415
yemax <egormaksimchuk@gmail.com>
2018-01-26 22:57:55 +0200

    Add new methods

--- src/main/java/jdbctutorial/application/dao/impl/DeveloperDAOImpl.java
+++ src/main/java/jdbctutorial/application/dao/impl/DeveloperDAOImpl.java
@@ -1,13 +1,15 @@
 package jdbctutorial.application.dao.impl;
 
-import jdbctutorial.application.utils.ConnectionUtil;
 import jdbctutorial.application.dao.DeveloperDAO;
 import jdbctutorial.application.model.Developer;
+import jdbctutorial.application.utils.ConnectionUtil;
 
 import java.math.BigDecimal;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeveloperDAOImpl implements DeveloperDAO {
@@ -33,24 +35,88 @@ public class DeveloperDAOImpl implements DeveloperDAO {
                     .withSalary(salary);
         }
 
+        try {
+            statement.getConnection().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return developer;
     }
 
     @Override
     public List<Developer> getAll() {
-        return null;
+        String sql = "SELECT * FROM developers";
+        List<Developer> developersList = new ArrayList<>();
+        Statement statement = null;
+
+        try {
+            statement = ConnectionUtil.getConnection().createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Developer Developer = new Developer();
+                Developer.setId(resultSet.getLong("id"));
+                Developer.setFirstName(resultSet.getString("first_name"));
+                Developer.setLastName(resultSet.getString("last_name"));
+                Developer.setSpecialty(resultSet.getString("specialty"));
+                Developer.setSalary(resultSet.getBigDecimal("salary"));
+                developersList.add(Developer);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.getConnection().close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return developersList;
     }
 
     @Override
     public void save(Developer developer) throws SQLException {
-        String sql = "INSERT INTO developers(id, first_name, last_name, specialty, salary) VALUES \t\n" +
-                "\t(1, 'John', 'Smith',   'C++', 3000.00);";
-        Statement statement = ConnectionUtil.getConnection().createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
+        PreparedStatement preparedStatement = null;
 
+        String sql = "INSERT INTO Developer (id, first_name, last_name, specialty, salary) VALUES (?,?,?,?,?)";
 
+        try {
+            preparedStatement = ConnectionUtil.getConnection().prepareStatement(sql);
 
+            preparedStatement.setLong(1, developer.getId());
+            preparedStatement.setString(2, developer.getFirstName());
+            preparedStatement.setString(3, developer.getLastName());
+            preparedStatement.setString(4, developer.getSpecialty());
+            preparedStatement.setBigDecimal(5, developer.getSalary());
+
+            preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement.getConnection() != null) {
+                preparedStatement.getConnection().close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
     }
 
     @Override
